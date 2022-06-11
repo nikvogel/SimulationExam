f487655dc358b56b08bf96446d1e30adb9808961
Niklas Vogel <niklas.vogel@outlook.com>
2022-06-11 22:26:05 +0200

    Improve heuristic

--- MetaHeuristic/main.py
+++ MetaHeuristic/main.py
@@ -5,6 +5,7 @@ from numpy.random import rand
 from numpy.random import randint
 from numpy.random import seed
 from matplotlib import pyplot
+import pandas as pd
 
 
 # objective function
@@ -53,57 +54,79 @@ def feasible_solution(x, demand):
 
 def neighbor(x, y, z, d, g):
     x_new = x.copy()
-    rand_period = randint(1, len(x) + 1)
-    print(f'x:{x}')
-    print(f'period{rand_period}')
-    current_x_t = x[rand_period - 1]
-    if current_x_t > 0:
-        if rand_period == len(x):
-            max_postpone = 0
-        else:
-            demand_till_period = d[0:rand_period].sum()
-            production_till_period = x[0:rand_period].sum()
-            max_postpone_prod_demand = production_till_period - demand_till_period
-            max_postpone_spare_capacity = (len(x) - rand_period) * g - x[rand_period: len(x)].sum()
-            max_postpone = min(max_postpone_prod_demand, max_postpone_spare_capacity)
-        print(f'postpone: {max_postpone}')
-
-        if rand_period == 1:
-            max_forward = 0
-        else:
-            max_forward = (rand_period - 1) * g - x[0:rand_period - 1].sum()
-        print(f'forward: {max_forward}')
-        max_remove = min(current_x_t, max_forward + max_postpone)
-        remove_x_t = randint(0, max_remove + 1)
-        print(f'remove {remove_x_t}')
-        rand_case = rand()
-        if rand_case < 0.4:
-            # postpone
-            print('postpone')
-            postpone_x = min(remove_x_t, max_postpone)
-            new_x = postpone(x, postpone_x, rand_period, g, True)
-            rest = remove_x_t - postpone_x
-            if rest > 0:
-                new_x = forward(new_x, rest, rand_period, g, True)
-        elif rand_case < 0.4:
-            # forward
-            print('forward')
-            forward_x = min(remove_x_t, max_forward)
-            new_x = postpone(x, forward_x, rand_period, g, True)
-            rest = remove_x_t - forward_x
-            if rest > 0:
-                new_x = postpone(new_x, rest, rand_period, g, True)
-        else:
-            print('mix')
-            postpone_x = randint(0, min(max_postpone, remove_x_t) + 1)
-            print(f'postpone {postpone_x}')
-            forward_x = randint(0, min(max_forward, remove_x_t - postpone_x) + 1)
-            print(f'forward {forward_x}')
-            x_new = postpone(x, postpone_x, rand_period, g, True)
-            x_new = forward(x_new, forward_x, rand_period, g, True)
-    print(f'd {d}')
+    current_x_t = 0
+    while current_x_t < 1:
+        rand_period = randint(1, len(x) + 1)
+        current_x_t = x[rand_period - 1]
+    random_case = rand()
+    if random_case < 0.3 and temp < 0.3*start_temp:
+        current_x_t2 = 0
+        second_period = 0
+        while current_x_t2 < 1 and rand_period != second_period:
+            second_period = randint(1, len(x) + 1)
+            current_x_t2 = x[second_period - 1]
+        if rand_period > second_period: # forward production
+            max_forward = min(g-x[second_period], current_x_t2)
+            if max_forward > 0:
+                x_forward = randint(1, max_forward+1)
+                x_new[rand_period] = x[rand_period] - x_forward
+                x_new[second_period] = x[second_period] + x_forward
+        else: # postpone production
+            max_postpone_production = min(g-x[second_period], x[rand_period])
+            max_postpone_demand = x[rand_period]
+            for t in range(rand_period, second_period+1):
+                demand_till_period = d[0:t].sum()
+                production_till_period = x[0:t].sum()
+                max_postpone_demand = min(production_till_period - demand_till_period, max_postpone_demand)
+            max_postpone = min(max_postpone_demand, max_postpone_production)
+            if max_postpone > 0:
+                x_postpone = randint(1, max_postpone + 1)
+                x_new[rand_period] = x[rand_period] - x_postpone
+                x_new[second_period] = x[second_period] + x_postpone
+    if rand_period == len(x):
+        max_postpone = 0
+    else:
+        demand_till_period = d[0:rand_period].sum()
+        production_till_period = x[0:rand_period].sum()
+        max_postpone_prod_demand = production_till_period - demand_till_period
+        max_postpone_spare_capacity = (len(x) - rand_period) * g - x[rand_period: len(x)].sum()
+        max_postpone = min(max_postpone_prod_demand, max_postpone_spare_capacity)
+    if rand_period == 1:
+        max_forward = 0
+    else:
+        max_forward = (rand_period - 1) * g - x[0:rand_period - 1].sum()
+    max_remove = min(current_x_t, max_forward + max_postpone)
+    if max_remove > 0:
+        remove_x_t = randint(1, max_remove + 1)
+        if temp > 5:
+            case = rand()
+            if case < 0.3:
+                remove_x_t = max_remove
+    else:
+        remove_x_t = 0
+    rand_case = rand()
+    if rand_case < 0.5:
+        # postpone
+        postpone_x = min(remove_x_t, max_postpone)
+        x_new = postpone(x, postpone_x, rand_period, g, True)
+        rest = remove_x_t - postpone_x
+        if rest > 0:
+            x_new = forward(x_new, rest, rand_period, g, True)
+    elif rand_case < 1:
+        # forward
+        forward_x = min(remove_x_t, max_forward)
+        x_new = forward(x, forward_x, rand_period, g, True)
+        rest = remove_x_t - forward_x
+        if rest > 0:
+            x_new = postpone(x_new, rest, rand_period, g, True)
+    else:
+        postpone_x = randint(0, min(max_postpone, remove_x_t) + 1)
+        forward_x = randint(0, min(max_forward, remove_x_t - postpone_x) + 1)
+        x_new = postpone(x, postpone_x, rand_period, g, True)
+        x_new = forward(x_new, forward_x, rand_period, g, True)
     y_new, z_new = feasible_solution(x_new, d)
     return x_new, y_new, z_new
+
 
 
 # simulated annealing algorithm
@@ -117,6 +140,7 @@ def simulated_annealing(cp, cf, cs, d, g, n_iterations, temp):
     scores = list()
     checked_scores = list()
     checked_x = list()
+    improvement_iterations = list()
 
     # evaluate the initial point
     best_eval = objective(x_best, y_best, z_best, cp, cf, cs)
@@ -140,13 +164,11 @@ def simulated_annealing(cp, cf, cs, d, g, n_iterations, temp):
             x_best, y_best, z_best, best_eval = x_cand, y_cand, z_cand, candidate_eval
             # keep track of scores
             best_scores.append(best_eval)
-            print('Hola!')
-            # report progress
-            # print('>%d f(%s) = %.5f' % (i, x_best,y_best,z_best, best_eval))
+            improvement_iterations.append(i)
         # difference between candidate and current point evaluation
         diff = candidate_eval - curr_eval
         # calculate temperature for current epoch
-        t = temp / float(i + 1)
+        t = temp * (0.8**i)
         # calculate metropolis acceptance criterion
         metropolis = exp(-diff / t)
         # check if we should keep the new point
@@ -154,17 +176,18 @@ def simulated_annealing(cp, cf, cs, d, g, n_iterations, temp):
             # store the new current point
             x_curr, y_curr, z_curr, curr_eval = x_cand, y_cand, z_cand, candidate_eval
         scores.append(curr_eval)
-    return [x_best, y_best, z_best, best_eval, best_scores, scores, checked_scores, checked_x]
+    return [x_best, y_best, z_best, best_eval, best_scores, scores, checked_scores, checked_x, improvement_iterations]
 
 
 # seed the pseudorandom number generator
-seed(1)
+seed(0)
 
 # define the total iterations
 n_iterations = 1000
 
 # initial temperature
-temp = 10
+start_temp = 1
+temp = 1
 
 # cost of production in periods t
 cp = asarray([3, 4, 3, 4, 4, 5])
@@ -175,10 +198,23 @@ d = asarray([6, 7, 4, 6, 3, 8])
 g = 10
 
 # perform the simulated annealing search
-best_x, best_y, best_z, score, best_scores, scores, checked_scores, checked_x = simulated_annealing(cp, cf, cs, d, g, n_iterations, temp)
-print('Done!')
-print(f'x:{best_x}, y: {best_y}, z:{best_z}')
-print(f'score: {score}')
+best_score_per_seed = list()
+iterrations_for_best_per_seed = list()
+
+for i in range(100):
+    seed(i)
+    best_x, best_y, best_z, score, best_scores, scores, checked_scores, checked_x, improvement_iterations = simulated_annealing(cp, cf, cs, d, g, n_iterations, temp)
+    best_score_per_seed.append(score)
+    iterrations_for_best_per_seed.append(improvement_iterations[-1])
+    print('Done!')
+    print(f'x:{best_x}, y: {best_y}, z:{best_z}')
+    print(f'score: {score}')
+
+print(f'Best average: {np.mean(best_score_per_seed)}')
+print(f'Max score: {max(best_score_per_seed)}')
+print(f'Iterations average: {np.mean(iterrations_for_best_per_seed)}')
+
+
 # print('f(%s) = %f' % (best_x, best_y, best_z, score))
 
 # line plot of best scores
